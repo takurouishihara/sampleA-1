9c2379f71a6bd8c4a580e4b273d33da9befd533d
takurouishihara <k19008@std.nagaokauniv.ac.jp>
2022-10-24 10:34:58 +0900

    deleteが出来ません2

---
+++ Downloads/ControllerB/ControllerB/src/main/java/Task72/ControllerB/ControllerBApplication.java
@@ -0,0 +1,13 @@
+package Task72.ControllerB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerBApplication.class, args);
+	}
+
+}
---
+++ Downloads/ControllerB/ControllerB/src/main/java/Task72/ControllerB/NameController.java
@@ -0,0 +1,34 @@
+package Task72.ControllerB;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public  class NameController {
+    @GetMapping("/names")
+    public List<String> getNames() {
+        return List.of("koyama", "tanaka");
+    }
+
+
+    @PostMapping("/names")
+    public ResponseEntity<String> create(@RequestBody CreateForm form) {
+        // 登録処理は省略
+
+        URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
+                .path("/names/id") // id部分は実際に登録された際に発⾏したidを設定する
+                .build()
+                .toUri();
+        return ResponseEntity.created(url).body("name successfully created");
+    }
+    @PatchMapping("/names")
+    public ResponseEntity<Map<String, String>> update(@RequestBody CreateForm form) {
+        // 更新処理は省略
+        return ResponseEntity.ok(Map.of("message", "name successfully updated"));
+    }
+
+}
